27d456be4743440cb22cae43a14c2ae0f8f26c52
Ducvuive2 <82598608+Ducvuive2@users.noreply.github.com>
2021-06-30 20:37:41 +0700

    Revert "Merge branch 'main' of https://github.com/Ducvuive2/QuanLiBanHang_BanHoanChinh into main"
    This reverts commit a262005fc8a7e1e6b1f03f12aa3be51bc3d0cbe2, reversing
    changes made to 0093aa898179e7ad513660fb68211b1d94022a6f.

--- QuanLiBanHang_08/src/Util/MyInstance.java
+++
@@ -1,9 +0,0 @@
-package test.Util;
-
-public class MyInstance {
-    public final static Integer IS_ADD=1;
-    public final static Integer IS_EDIT=2;
-    public final static Integer IS_REMOVE=3;
-    public final static Integer IS_FIND=4;
-
-}
